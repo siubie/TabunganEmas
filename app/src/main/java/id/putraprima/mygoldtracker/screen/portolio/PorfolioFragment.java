b76abf3781e534ba3e01f4f6fc789e038bd3bac2
Putra Prima Arhandi <putraprima@gmail.com>
2020-12-02 22:35:31 +0700

    Runtime Permission

--- app/src/main/java/id/putraprima/mygoldtracker/screen/portolio/PorfolioFragment.java
+++ app/src/main/java/id/putraprima/mygoldtracker/screen/portolio/PorfolioFragment.java
@@ -1,5 +1,6 @@
 package id.putraprima.mygoldtracker.screen.portolio;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import id.putraprima.mygoldtracker.R;
 
@@ -20,6 +29,25 @@ public class PorfolioFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Dexter.withContext(requireContext())
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Toast.makeText(getContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        Toast.makeText(getContext(), "Permission Rationale", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .check();
     }
 
     @Override
@@ -28,4 +56,6 @@ public class PorfolioFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_porfolio, container, false);
     }
+
+
 }
